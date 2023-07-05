64259d8cbee2bb26cbba15bddea358b619eace55
golah-777 <128902542+golah-777@users.noreply.github.com>
2023-07-05 14:07:10 +0200

    added more js

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ class Note {
 class App{
   constructor(){
     this.notes = [];
-    this.$activateForm = document.querySelector(".active-form")
+    this.$activateForm = document.querySelector(".active-form");
     this.$notetitlepin = document.querySelector(".notetitle-pin");
-    this.$footer = document.querySelector(".footer")
+    this.$footer = document.querySelector(".footer");
+    this.$btndiv = document.querySelector(".btn-div ");
+    this.$formiconssec = document.querySelector(".form-icons-sec");
+    this.$clearBtn =  document.querySelector(".clear-btn");
+    this.$noteTitle = document.querySelector(".note-title");
+    this.noteText = document.querySelector(".note-content");
 
     this.addEventListeners();
   }
@@ -23,13 +28,43 @@ class App{
   }
 
   handleFormClick(event){
+   const activateFormClicked = this.$activateForm.contains(event.target);
+   const clearbtnFormClicked =  this.$clearBtn.contains(event.target);
+   const noteTitlePin = this.$notetitlepin.contains(event.target) ;
+   const footerform = this.$footer.contains(event.target);
+   const title =  this.$noteTitle.value;
+   const text = this.noteText.value;
+   console.log(cuid())
     
+   if(activateFormClicked){
+    this.openForm();
+
+   } else if((!activateFormClicked && !noteTitlePin && !footerform) || clearbtnFormClicked){
+    this.closeForm();
+    this.addNote({title, text})
+   }
+  }
+
+  openForm(){
+    this.$notetitlepin.style.display = 'inline';
+    this.$footer.style.display = 'inline';
+    this.$footer.style.display = 'flex';
+    this.$btndiv.style.display = 'inline-block';
+    this.$formiconssec.style.display = 'none'
+  }
+
+  closeForm(){
+    this.$notetitlepin.style.display = 'none';
+    this.$footer.style.display = 'none';
+    this.$formiconssec.style.display = 'inline';
+    this.$formiconssec.style.display = 'flex';
   }
 
 
-  addNote(id,{title,text}) {
-    const newNote = new Note(id, title, text);
-    this.notes = [...this.notes, newNote ]
+  addNote({title,text}) {
+    const newNote = new Note(cuid(), title, text);
+    this.notes = [...this.notes, newNote ];
+    this.displayNotes();
   }
 
   editNote(id, {title, text}){
