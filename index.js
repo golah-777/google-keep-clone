5274802504b8db78c2bab97a9bbc425260cb30ae
golah-777 <128902542+golah-777@users.noreply.github.com>
2023-07-10 11:06:39 +0200

    added more js

--- index.js
+++ index.js
@@ -15,14 +15,16 @@ class App{
     this.$btndiv = document.querySelector(".btn-div ");
     this.$formiconssec = document.querySelector(".form-icons-sec");
     this.$clearBtn =  document.querySelector(".btn-div");
-    // this.$noteTitle = document.querySelector(".note-title");
     this.$noteTitle= document.querySelector(".title-placeholder");
-    // this.noteText = document.querySelector(".note-content");
     this.$noteText = document.querySelector(".active-form");
+    // this.noteText = document.querySelector(".note-content");
     this.$notes = document.querySelector(".notes");
     this.$formActive = document.querySelector("#form-active");
     this.$modal = document.querySelector(".modal");
     this.$modalForm = document.querySelector(".modal-form");
+    this.$modalTitle = document.querySelector(".modal-title");
+    this.$modalNote = document.querySelector(".modal-note")
+
    
     this.addEventListeners();
   }
@@ -124,10 +126,16 @@ class App{
   openModal(event){
 
     const modalFormClicked = this.$modalForm.contains(event.target);
-
-
-    if(event.target.closest(".note")){
-      this.$modal.style.display = 'inline'
+    const $selectedNote = event.target.closest(".note");
+    const $modalTitleValue = this.$notes.querySelector("#title");
+    const $modalTextValue = this.$notes.querySelector("#text");
+    const note = this.$notes.querySelector('.note')
+ 
+    if($selectedNote){
+      this.$modal.style.display = 'inline';
+      this.$modalTitle.value =  $modalTitleValue.textContent;
+      this.$modalNote.value = $modalTextValue.textContent;
+      
     } else if (!modalFormClicked){
       this.$modal.style.display = 'none'
     }
@@ -142,14 +150,14 @@ class App{
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
         </div>
         <div class="title-note">
-          <span>${note.title}</span>
+          <span id="title">${note.title}</span>
           <div class="icons">
             <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M14,4v5c0,1.12,0.37,2.16,1,3H9c0.65-0.86,1-1.9,1-3V4H14 M17,2H7C6.45,2,6,2.45,6,3c0,0.55,0.45,1,1,1c0,0,0,0,0,0l1,0v5 c0,1.66-1.34,3-3,3v2h5.97v7l1,1l1-1v-7H19v-2c0,0,0,0,0,0c-1.66,0-3-1.34-3-3V4l1,0c0,0,0,0,0,0c0.55,0,1-0.45,1-1 C18,2.45,17.55,2,17,2L17,2z"/></g></svg>
             <div class="tooltip">Pin note</div>
           </div>
         </div>
         <div class="text-note">
-          <span>${note.text}</span>
+          <span id="text">${note.text}</span>
         </div>
         <div class="footer-note">
           <div class="icons">
